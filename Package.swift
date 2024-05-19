a1ab2661d90d7eb70fc2045ac51ffa608bb9c7f3
Treata11 <treata11@yahoo.com>
2024-05-19 16:04:16 +0330

    Renamed the main directories of the package to `Bass` & `CBass` instead of `C` & `Swift`.

--- Package.swift
+++ Package.swift
@@ -5,30 +5,36 @@ import PackageDescription
 
 let package = Package(
     name: "CBass",
-    platforms: platforms,
+    
+    platforms: [.iOS(.v13), .macOS(.v12)],
+    
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CBass",
 //            type: libType,
-            targets: ["Bass", "CBass"]
+            targets: ["Bass"]
         ),
     ],
+    
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Bass",
-            dependencies: ["CBass"],
-            path: "Sources/Swift"
+            dependencies: [
+                .target(name: "CBass")
+            ],
+            path: "Sources/CBass"
         ),
+        
         .target(
             name: "CBass",
             dependencies: [
                 .target(name: "bass", condition: .when(platforms: [.iOS])),
                 .target(name: "libbass", condition: .when(platforms: [.macOS]))
             ],
-            path: "Sources/C"
+            path: "Sources/Bass"
         ),
         /// The **iOS** binary target
         .binaryTarget(
@@ -42,35 +48,3 @@ let package = Package(
         )
     ]
 )
-
-// MARK: -
-
-fileprivate var platforms: [SupportedPlatform] = [.iOS(.v13), .macOS(.v12)]
-
-// A known issue for using #if os() conditions in swift packages:
-// https://github.com/apple/swift-package-manager/issues/4388
-// https://forums.swift.org/t/adding-platform-specific-dependency-to-multi-platform-swift-package/49645/10
-
-fileprivate var libType: Product.Library.LibraryType {
-    #if os(iOS)
-        return .static
-    #elseif os(macOS)
-        return .dynamic
-    #endif
-}
-
-fileprivate var binaryTarget: Target {
-    #if os(macOS)
-        .binaryTarget(
-            name: "bass",
-            path: "./Frameworks/libbass.xcframework"
-        )
-    #elseif os(iOS)
-        .binaryTarget(
-            name: "bass",
-            path: "./Frameworks/bass.xcframework"
-        )
-    #else
-        fatalError("BASS; Unsupported platform")
-    #endif
-}
